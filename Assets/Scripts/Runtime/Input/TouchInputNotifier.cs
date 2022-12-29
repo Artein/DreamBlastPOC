7e9ae25180fec4a92201ead5d77419715c128a50
Artem Sukhliak <artyom.suhlyak@gmail.com>
2022-12-29 12:41:22 +0200

    Reformat

--- Assets/Scripts/Runtime/Input/TouchInputNotifier.cs
+++ Assets/Scripts/Runtime/Input/TouchInputNotifier.cs
@@ -11,39 +11,49 @@ namespace Game.Input
         
         void ITickable.Tick()
         {
-            bool receivedTouch = false;
             Vector2 position = Vector2.zero;
-            
+
+            if (TryGetTouchPosition_Mobile(ref position) || 
+                TryGetTouchPosition_Desktop(ref position))
+            {
+                TouchBegan?.Invoke(position);
+            }
+        }
+
+        private static bool TryGetTouchPosition_Mobile(ref Vector2 position)
+        {
             if (Application.isMobilePlatform && UnityEngine.Input.touchCount > 0)
             {
                 foreach (var touch in UnityEngine.Input.touches)
                 {
                     if (touch.phase == TouchPhase.Began)
                     {
-                        receivedTouch = true;
                         position = touch.position;
-                        break;
+                        return true;
                     }
                 }
             }
-            else if (UnityEngine.Input.GetMouseButtonDown(0))
+
+            return false;
+        }
+
+        private static bool TryGetTouchPosition_Desktop(ref Vector2 position)
+        {
+            if (UnityEngine.Input.GetMouseButtonDown(0))
             {
                 position = UnityEngine.Input.mousePosition;
                 // Note: Input.mousePosition reports the position of the mouse even when it is not inside the Game View, such as when Cursor.lockState
                 // is set to CursorLockMode.None. When running in windowed mode with an unconfined cursor, position values smaller than 0 or greater
                 // than the screen dimensions (Screen.width,Screen.height) indicate that the mouse cursor is outside of the game window.
-                if (position.x >= 0 && position.y >= 0 && position.x <= Screen.width && position.y <= Screen.height)
+                if (position is { x: >= 0, y: >= 0 } && position.x <= Screen.width && position.y <= Screen.height)
                 {
-                    receivedTouch = true;
+                    return true;
                 }
             }
 
-            if (receivedTouch)
-            {
-                TouchBegan?.Invoke(position);
-            }
+            return false;
         }
-
+        
         public delegate void TouchBeganHandler(Vector2 pixelCoordinates);
     }
 }
